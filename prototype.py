9b2f55275b3db05f4216e88c4ae1a81911a73d76
Nsheppard01 <alexthegreater@gmail.com>
2014-04-29 12:09:49 -0700

    Update prototype.py
    made variables into strings.

--- prototype.py
+++ prototype.py
@@ -1,47 +1,47 @@
 print "This is the elivator, type LOOK at any time for more information."
-room = elivator
+room = 'elivator'
 
-while room == elivator:
+while room == 'elivator':
 	command = raw_input("Enter comand, you can go NORTH")
 	
 	if command=='north':
 		print "You enter the Multivac IO room."
-		room = io
+		room = 'io'
 	elif command=='look':
 		print "You are in an old industrial elivator."
 		print "The Multiac IO room lies through a door to the NORTH."
 	else: print "Invalid command."
 	
-while room == io:
+while room == 'io':
 	command = raw_input("Enter comand, you can go NORTH or SOUTH")
 	
 	if command=='north':
 		print "You enter the Multivac controll unit room."
-		room = controll
+		room = 'controll'
 	elif command=='south':
 		print "You enter the elivator."
-		room = elivator
+		room = 'elivator'
 	elif command=='look':
 		print "You are in the Input/output room for Multivac."
 		print "The floor is covered in reems of paper printouts, and hundreds of lights flash on various consoles."
 		print "The controll unit lies to the NORTH, an elivator lies to the SOUTH."
 	else: print "Invalid command."
 	
-while room == controll:
+while room == 'controll':
 	command = raw_input("Enter comand, you can go NORTH, SOUTH, EAST, or WEST")
 	
 	if command=='north':
 		print "You enter the Multivac Logic unit room."
-		room = logic
+		room = 'logic'
 	elif command == 'south':
 		print "You enter the Multivac IO room."
-		room = io
+		room = 'io'
 	elif command == 'east':
 		print "You enter the Data Memory room."
-		room = data
+		room = 'data'
 	elif command == 'west':
 		print "You enter the Instruction Memory room."
-		room = instruction
+		room = 'instruction'
 	elif command =='look':
 		print "You are in the heart of Multivac. The controll unit towers above you. A mass of cables snake"
 		print "in all directions, to the NORTH they lead to the ALU room, to the SOUTH is the IO room,"
