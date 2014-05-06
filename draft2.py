c817a50a6d50839af97a6ea1e08e1e40c60692ca
Nsheppard01 <alexthegreater@gmail.com>
2014-05-06 11:26:43 -0700

    Create draft2.py
    laid down goals.

---
+++ draft2.py
@@ -0,0 +1,5 @@
+#Second revision of the walking game
+#Instead of while loops, rooms will be defined by dictonaries containing all the relivant information about the room
+#While the player is in the room, the game will display that rooms discriptive text, and allow the player to exicute comands specific to that room.
+#Rooms can be added and removed without changing the overall code.
+#Idealy the room database will be in a seperate file from the game's engine.
