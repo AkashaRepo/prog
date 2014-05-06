4c0ece2e135135d0864a154adf30f9f92c723d0a
claudefaustus <claude.human.person@gmail.com>
2014-05-06 13:54:17 -0700

    Update inventorygame.py

--- inventorygame.py
+++ inventorygame.py
@@ -100,22 +100,26 @@ while len(inventory) <= 2:
         prompt = ">"
         next = raw_input(prompt)
     else:
-        while len(inventory) == 2:
-            if sum(inventory) == 3:
-                print "you have made a drawing"
-            elif sum(inventory) == 30:
-                print "you have made a patch on your shirt"
-            elif sum(inventory) == 150:
-                print "you have made suds"
-            elif sum(inventory) == 51:
-                print "you have made a sundial"
-            elif sum(inventory) == 60:
-                print "you have made a soap sculpture"
-            elif sum(inventory) == 110:
-                print "you have made a compass"
-            elif sum(inventory) == 70:
-                print "you have made a deer scare"
-            else:
-                print "the items you've picked up can't be combined"
-                inventory = []
+        print "I don't understand %s" % next
+        prompt = '>'
+        next = raw_input(prompt)
+
+while len(inventory) == 2:
+        if sum(inventory) == 3:
+            print "you have made a drawing"
+        elif sum(inventory) == 30:
+            print "you have made a patch on your shirt"
+        elif sum(inventory) == 150:
+            print "you have made suds"
+        elif sum(inventory) == 51:
+            print "you have made a sundial"
+        elif sum(inventory) == 60:
+            print "you have made a soap sculpture"
+        elif sum(inventory) == 110:
+            print "you have made a compass"
+        elif sum(inventory) == 70:
+            print "you have made a deer scare"
+        else:
+            print "the items you've picked up can't be combined"
+            inventory = []
     
