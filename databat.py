5065aa66f49d9c3425f1e510ec29a498b0178b41
Nat <alexthegreater@gmail.com>
2014-05-06 12:52:36 -0700

    finished databat

--- databat.py
+++ databat.py
@@ -4,11 +4,35 @@
 #Rooms can be added and removed without changing the overall code.
 #Idealy the room database will be in a seperate file from the game's engine.
 
-Elivator = {
-	'look':"You are inside a rusty old industrial elivator.",
-	'exits': {'U':'exit','N':'IO'}
-	}
-IO = {
-	'look':"You are in the Input/output room for Multivac. The floor is covered in reems of paper printouts, and hundreds of lights flash on various consoles.",
-	'exits':{'S':'Elivator','N':'Control'}
-	}
+rooms = {
+	'elivator':{
+		'look':"You are inside a rusty old industrial elivator.",
+		'exits':{'U':'exit','N':'io'}
+		'contents':[]
+		}
+	"io":{
+		'look':"You are in the Input/Output room for Multivac. The floor is covered in reems of paper printouts, and hundreds of lights flash on various consoles.",
+		'exits':{'S':'Elivator','N':'Control'}
+		'contents':[]
+		}
+	'control':{
+		'look':"You are in the heart of Multivac. The control unit towers above you. A mass of cables snake in all directions",
+		'exits':{'N':'logic','S':'io','E':'data','W':'instruction'}
+		'contents':[]
+		}
+	'logic':{
+		'look':"You are in Multivac's Logic room. At the far end of the room a row of massive logic units.",
+		'exits':{'S':'control'}
+		'contents':[]	
+		}
+	'data':{
+		'look':"The eastern side of the bunker is a cast arcade lined with rows and rows of data memmory units.",
+		'exits':{'W':'control'},
+		'contents':[]
+		}
+	'instruction':{
+		'look':"The western side of the bunker is a vast arcade lined with rows and rows of instruction memmory units.",
+		'exits':{'E':'control'}
+		'contents':[]
+		}
+}
