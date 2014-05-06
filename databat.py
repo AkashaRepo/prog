9596096206247e67b402d81d7f6c3ae5b33585b1
Nsheppard01 <alexthegreater@gmail.com>
2014-05-06 11:49:04 -0700

    Update and rename draft2.py to databat.py
    this file is now the database for the second draft.

--- databat.py
+++ databat.py
@@ -1,5 +1,14 @@
-#Second revision of the walking game
+#Database for the Second revision of the walking game
 #Instead of while loops, rooms will be defined by dictonaries containing all the relivant information about the room
 #While the player is in the room, the game will display that rooms discriptive text, and allow the player to exicute comands specific to that room.
 #Rooms can be added and removed without changing the overall code.
 #Idealy the room database will be in a seperate file from the game's engine.
+
+Elivator = {
+	'look':"You are inside a rusty old industrial elivator.",
+	'exits': {'U':'exit','N':'IO'}
+	}
+IO = {
+	'look':"You are in the Input/output room for Multivac. The floor is covered in reems of paper printouts, and hundreds of lights flash on various consoles.",
+	'exits':{'S':'Elivator','N':'Control'}
+	}
