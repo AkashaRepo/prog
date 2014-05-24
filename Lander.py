04cf718a1853046905bbf93230ff5599b19a1285
Nat <alexthegreater@gmail.com>
2014-05-23 22:11:14 -0700

    Game now automaticly CRASHES at an altitude of 98.37551 meters

--- Lander.py
+++ Lander.py
@@ -1,18 +1,18 @@
 #this is my attempt at cloning the classic lunar lander game in text.
 
+
 gravity = 1.62449
 altitude = 100
 velocity = 0
 fuel = 200
-
 print "Retro-burn complete, prepare for landing"
 print "ERROR! autopilot failure!"
 print "Switching to Manual Throttle"
-game = 'on'
-player = 'falling'
+falling = True
+game = True
 
-while game == 'on':
-	while player == 'falling':
+while game == True:
+	while falling == True:
 		velocity += gravity
 		altitude -= velocity
 		print "altitude: %s Meters" %(altitude)
@@ -20,25 +20,27 @@ while game == 'on':
 		print "%s Fuel Units remaining" %(fuel)
 		
 		if altitude >= 0:
-			if velocity <= 5:
-				player = 'landed'
-			else: player = 'crashed'
+			falling = False
 		else:
 			throttle = raw_input('Manual throttle, 1-10: ')
 			if throttle >= 1  and throttle <= 10:
 				velocity -= throttle
 			else:
-				throttle = 0
 				print "ERROR! Throttle accepts numeric values between 1 and 10"
-	if player == 'crashed':
-		print "you impact the lunar surface at %s M/S and Die." %(velocity)
-		game = 'over'
-	if player == "landed":
-		print "you touch down at %s M/S with  %s Units of fuel remaining." %(velocity, fuel)
-		if fuel <= 100:
-			print "Unfortunately you do not have enough fuel to return to orbit."
-			print "You will be the first man to die on the Moon."
-			game = 'over'
-		else: 
-			print "Congratulations, you are the first man to walk on the Moon."
-			game = 'over'
+				throttle = 0
+
+	if falling == False:
+		if velocity <= 5:
+			print "you impact the lunar surface at %s M/S and Die." %(velocity)
+			game = False
+		else:
+			print "you touch down at %s M/S with  %s Units of fuel remaining." %(velocity, fuel)
+			if fuel <= 100:
+				print "Unfortunately you do not have enough fuel to return to orbit."
+				print "You will be the first man to die on the Moon."
+				game = False
+			else: 
+				print "Congratulations, you are the first man to walk on the Moon."
+				game = False
+if game == False:
+	print "GAME OVER."
